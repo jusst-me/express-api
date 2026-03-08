1f6e025ef5d878e9cbab967aef663481e55db737
Justin van Dijk <jvdijk@goodnews.nl>
2026-03-08 18:20:45 +0100

    feat: Integrate Express framework and update dependencies
    - Added Express as a dependency and set up a basic server in index.ts.
    - Updated dotenv to version 16.6.1 and added @types/express as a dev dependency.
    - Updated eslint-config-jvd and ts-jest to their latest versions.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.info('Hello, world!');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(3000, () => {
+  console.info('Server is running on port 3000');
+});
